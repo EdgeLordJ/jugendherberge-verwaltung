461b9bdd0beac725ada1f3dfa39d931485daa5da
joudi.khalil@student.htl-rankweil.at <joudi.khalil@student.htl-rankweil.at>
2024-10-09 10:29:51 +0000

    Created Startseite and Database_Server; Edited settings; Deleted Form1

---
+++ client_code/Startseite/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import StartseiteTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+
+class Startseite(StartseiteTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.    
+    print(anvil.server.call("say_hello", "sauron"))
+    print(anvil.server.call('return_text_from_file'))
+
+    # items = []
+    # for x in anvil.server.call('return_text_from_file'):
+    #   items.append((x[1], x[0]))
+    self.drop_down_1.items = anvil.server.call('return_text_from_file', 'name, JID')
